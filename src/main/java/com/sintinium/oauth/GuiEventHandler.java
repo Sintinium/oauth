7d5d9773fd9ef91e876c2e38e6cf32e6834c2e72
Sintinium <sintinium@gmail.com>
2021-09-02 14:49:07 -0500

    Fixed issue causing servers to crash

---
+++ src/main/java/com/sintinium/oauth/GuiEventHandler.java
@@ -0,0 +1,63 @@
+package com.sintinium.oauth;
+
+import com.sintinium.oauth.gui.LoginTypeScreen;
+import com.sintinium.oauth.gui.TextWidget;
+import com.sintinium.oauth.login.LoginUtil;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiMultiplayer;
+import net.minecraftforge.client.event.GuiScreenEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Mod.EventBusSubscriber(modid = "oauth", value = Side.CLIENT)
+public class GuiEventHandler {
+    private static TextWidget statusText = new TextWidget(10 + 66 + 3, 12, "Status: offline");
+
+    @SubscribeEvent
+    public static void multiplayerScreenOpen(GuiScreenEvent.InitGuiEvent.Post event) {
+        if (!(event.getGui() instanceof GuiMultiplayer)) return;
+        GuiMultiplayer multiplayerScreen = (GuiMultiplayer) event.getGui();
+        try {
+//            Method addButtonMethod = ObfuscationReflectionHelper.findMethod(Screen.class, "addButton", Widget.class);
+//            Method addButtonMethod = ObfuscationReflectionHelper.findMethod(Screen.class, "func_230480_a_", Widget.class);
+            List<GuiButton> buttonList = new ArrayList<>();
+            GuiButton loginButton = new GuiButton(29183, 10, 6, 66, 20, "Oauth Login");
+            // p_onPress_1_ ->
+            buttonList.add(loginButton);
+            Thread thread = new Thread(() -> {
+                boolean isOnline = LoginUtil.isOnline();
+                if (isOnline) {
+                    statusText.setText("Status: online");
+                    statusText.setColor(0x55FF55);
+                } else {
+                    statusText.setText("Status: offline");
+                    statusText.setColor(0xFF5555);
+                }
+            });
+            thread.start();
+
+            event.getButtonList().addAll(buttonList);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @SubscribeEvent
+    public static void multiplayerScreenDraw(GuiScreenEvent.DrawScreenEvent.Post event) {
+        if (!(event.getGui() instanceof GuiMultiplayer)) return;
+        statusText.draw(event.getGui());
+    }
+
+    @SubscribeEvent
+    public static void action(GuiScreenEvent.ActionPerformedEvent.Post event) {
+        if (!(event.getGui() instanceof GuiMultiplayer)) return;
+        if (event.getButton().id != 29183) return;
+        GuiMultiplayer multiplayerScreen = (GuiMultiplayer) event.getGui();
+        Minecraft.getMinecraft().displayGuiScreen(new LoginTypeScreen(multiplayerScreen));
+    }
+}
--- src/main/java/com/sintinium/oauth/OAuth.java
+++ src/main/java/com/sintinium/oauth/OAuth.java
@@ -1,20 +1,9 @@
 package com.sintinium.oauth;
 
-import com.sintinium.oauth.gui.LoginTypeScreen;
-import com.sintinium.oauth.gui.TextWidget;
-import com.sintinium.oauth.login.LoginUtil;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.gui.GuiButton;
-import net.minecraft.client.gui.GuiMultiplayer;
-import net.minecraftforge.client.event.GuiScreenEvent;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
-import java.util.ArrayList;
-import java.util.List;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(modid = "oauth", clientSideOnly = true)
@@ -22,53 +11,9 @@ public class OAuth {
     // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    private static TextWidget statusText = new TextWidget(10 + 66 + 3, 12, "Status: offline");
     public static boolean savePassword = false;
 
     public OAuth() {
         MinecraftForge.EVENT_BUS.register(this);
-    }
-
-    @SubscribeEvent
-    public void multiplayerScreenOpen(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (!(event.getGui() instanceof GuiMultiplayer)) return;
-        GuiMultiplayer multiplayerScreen = (GuiMultiplayer) event.getGui();
-        try {
-//            Method addButtonMethod = ObfuscationReflectionHelper.findMethod(Screen.class, "addButton", Widget.class);
-//            Method addButtonMethod = ObfuscationReflectionHelper.findMethod(Screen.class, "func_230480_a_", Widget.class);
-            List<GuiButton> buttonList = new ArrayList<>();
-            GuiButton loginButton = new GuiButton(29183, 10, 6, 66, 20, "Oauth Login");
-            // p_onPress_1_ ->
-            buttonList.add(loginButton);
-            Thread thread = new Thread(() -> {
-                boolean isOnline = LoginUtil.isOnline();
-                if (isOnline) {
-                    statusText.setText("Status: online");
-                    statusText.setColor(0x55FF55);
-                } else {
-                    statusText.setText("Status: offline");
-                    statusText.setColor(0xFF5555);
-                }
-            });
-            thread.start();
-
-            event.getButtonList().addAll(buttonList);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @SubscribeEvent
-    public void multiplayerScreenDraw(GuiScreenEvent.DrawScreenEvent.Post event) {
-        if (!(event.getGui() instanceof GuiMultiplayer)) return;
-        statusText.draw(event.getGui());
-    }
-
-    @SubscribeEvent
-    public void action(GuiScreenEvent.ActionPerformedEvent.Post event) {
-        if (!(event.getGui() instanceof GuiMultiplayer)) return;
-        if (event.getButton().id != 29183) return;
-        GuiMultiplayer multiplayerScreen = (GuiMultiplayer) event.getGui();
-        Minecraft.getMinecraft().displayGuiScreen(new LoginTypeScreen(multiplayerScreen));
     }
 }
