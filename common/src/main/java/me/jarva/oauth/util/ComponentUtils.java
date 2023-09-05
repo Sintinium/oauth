2b97bab7da4da483a242a45a31fbf983a87bda94
Jarva <4622609+Jarva@users.noreply.github.com>
2023-09-05 13:32:44 +0100

    fix: update ComponentUtils imports

--- common/src/main/java/me/jarva/oauth/util/ComponentUtils.java
+++ common/src/main/java/me/jarva/oauth/util/ComponentUtils.java
@@ -1,7 +1,12 @@
 package me.jarva.oauth.util;
 
+#if POST_MC_1_18_2
+import net.minecraft.network.chat.Component;
+import net.minecraft.network.chat.MutableComponent;
+#else
 import net.minecraft.network.chat.MutableComponent;
 import net.minecraft.network.chat.TextComponent;
+#endif
 
 public class ComponentUtils {
     public static MutableComponent literal(String text) {
